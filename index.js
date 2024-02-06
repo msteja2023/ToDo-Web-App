d23db9655c1356fa847e6e8dfb2feccc6163f330
msteja2023 <144339469+msteja2023@users.noreply.github.com>
2024-02-06 18:50:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBOKm1XcCJXkQoUl8SYsv8V4j36gi-4Zlw",
-    authDomain: "transactions-aaa00.firebaseapp.com",
-    projectId: "transactions-aaa00",
-    storageBucket: "transactions-aaa00.appspot.com",
-    messagingSenderId: "457514132328",
-    appId: "1:457514132328:web:8b72e1d6685432003a6cdd",
-    measurementId: "G-553EM034E6"
-  };
+  apiKey: "AIzaSyBOKm1XcCJXkQoUl8SYsv8V4j36gi-4Zlw",
+  authDomain: "transactions-aaa00.firebaseapp.com",
+  projectId: "transactions-aaa00",
+  storageBucket: "transactions-aaa00.appspot.com",
+  messagingSenderId: "457514132328",
+  appId: "1:457514132328:web:8b72e1d6685432003a6cdd",
+  measurementId: "G-553EM034E6"
+};
 
 firebase.initializeApp(firebaseConfig);
 
